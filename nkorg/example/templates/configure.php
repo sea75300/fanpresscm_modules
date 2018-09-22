c21db69bdbb5198ea901bb0c7c4647905dee2886
stefan <stefan@stefan-laptop>
2018-09-22 13:26:02 +0200

    Event modules/configure

--- nkorg/example/templates/configure.php
+++ nkorg/example/templates/configure.php
@@ -7,4 +7,20 @@
         <div class="col-6"><?php $theView->textInput("config[{$key}]")->setValue($value); ?></div>
     </div>
     <?php endforeach; ?>
+    
+    <div class="row no-gutters fpcm-ui-padding-md-tb">
+        <div class="col-6 align-self-center">Var 1</div>
+        <div class="col-6"><?php print $var1; ?></div>
+    </div>
+    
+    <div class="row no-gutters fpcm-ui-padding-md-tb">
+        <div class="col-6 align-self-center">Var 2</div>
+        <div class="col-6"><?php fpcmDump($var2); ?></div>
+    </div>
+    
+    <div class="row no-gutters fpcm-ui-padding-md-tb">
+        <div class="col-6 align-self-center">Var 3</div>
+        <div class="col-6"><?php print $var3; ?></div>
+    </div>
+    
 </div>
