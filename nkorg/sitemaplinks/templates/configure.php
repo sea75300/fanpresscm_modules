a4b9e0e1d5d2fce4825c3c17a5ca248ab7293ed8
stefan <stefan@stefan-pc>
2019-06-22 18:22:40 +0200

    newgui changes

--- nkorg/sitemaplinks/templates/configure.php
+++ nkorg/sitemaplinks/templates/configure.php
@@ -1,17 +1,13 @@
 <?php /* @var $theView fpcm\view\viewVars */ ?>
-<div class="row no-gutters fpcm-ui-full-height">
-    <div class="col-12">
-        <div class="fpcm-content-wrapper fpcm-ui-full-height">
-            <div class="fpcm-ui-tabs-general">
-                <ul>
-                    <li><a href="#tabs-config"><?php $theView->write('MODULE_NKORGSITEMAPLINKS_SITEMAPPATH'); ?></a></li>
-                </ul>            
+<div class="fpcm-content-wrapper">
+    <div class="fpcm-ui-tabs-general">
+        <ul>
+            <li><a href="#tabs-config"><?php $theView->write('MODULE_NKORGSITEMAPLINKS_SITEMAPPATH'); ?></a></li>
+        </ul>            
 
-                <div id="tabs-config">
-                    <div class="row no-gutters">
-                        <div class="col-12"><?php $theView->textInput("config[module_nkorgsitemaplinks_sitemappath]")->setValue($options['module_nkorgsitemaplinks_sitemappath']); ?></div>
-                    </div>
-                </div>
+        <div id="tabs-config">
+            <div class="row no-gutters">
+                <div class="col-12"><?php $theView->textInput("config[module_nkorgsitemaplinks_sitemappath]")->setValue($options['module_nkorgsitemaplinks_sitemappath']); ?></div>
             </div>
         </div>
     </div>
