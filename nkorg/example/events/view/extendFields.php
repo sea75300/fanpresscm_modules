67bff9fd68323efb9d2b0642883720403e1b9b96
stefan <stefan@stefan-pc>
2022-12-26 17:46:07 +0100

    Add example form Simple Form extension event

--- nkorg/example/events/view/extendFields.php
+++ nkorg/example/events/view/extendFields.php
@@ -7,7 +7,7 @@ final class extendFields extends \fpcm\modules\nkorg\example\events\eventBase  {
     final public function extendFormSmileysadd()
     {   
         $form = $this->data->getData();
-        $form->fields[] = (new \fpcm\view\helper\textInput('exampltetest'))->setText('Example Test')->setValue('ABC')->setIcon('check');
+        $form->fields[] = (new \fpcm\view\helper\textInput('smiley[example]'))->setText('Example Test')->setValue('ABC')->setIcon('check');
         $this->data->setData($form);
 
         return $this->data;
