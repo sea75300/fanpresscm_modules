11c66caa04f98100b6851aaf4566a60acb7930ee
stefan <stefan@stefan-tuxedo>
2024-05-13 19:22:28 +0200

    bugfix example module

--- nkorg/example/events/view/extendTabs.php
+++ nkorg/example/events/view/extendTabs.php
@@ -7,7 +7,12 @@ final class extendTabs extends \fpcm\modules\nkorg\example\events\eventBase  {
     final public function run()
     {        
         $this->data->tabs[] = (new \fpcm\view\helper\tabItem(md5(self::class)))
-                ->setText('Extend tabs item');
+            ->setText('Extend tabs item')
+            ->seturl(\fpcm\classes\tools::getFullControllerLink('ajax/logs/reload', [
+                'log' => 'nkorg-example',
+                'key' => $this->getModuleKey()
+            ]))
+            ->setDataViewId('logs');
 
         return (new \fpcm\module\eventResult)->setData($this->data);
     }
