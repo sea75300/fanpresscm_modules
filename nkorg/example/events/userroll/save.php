166375cfed65f6be690e47b52b4d7f45e356a208
stefan <stefan@stefan-tuxedo>
2025-06-08 14:07:41 +0200

    refactor event return values v5.3

--- nkorg/example/events/userroll/save.php
+++ nkorg/example/events/userroll/save.php
@@ -6,7 +6,8 @@ final class save extends \fpcm\modules\nkorg\example\events\eventBase {
 
     public function run() : \fpcm\module\eventResult 
     {
-        return (new \fpcm\module\eventResult())->setData( $this->data.' '. microtime(true).' '.$this->getModuleKey().' '. get_class($this) );
+        $this->data['codex'] .=  ' '. microtime(true).' '.$this->getModuleKey().' '. get_class($this);        
+        return (new \fpcm\module\eventResult())->setData($this->data);
     }
 
 }
